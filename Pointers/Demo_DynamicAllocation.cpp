a7837bf162b46705c2cb9378f7de3ca670d86aa9
Punvireakroth <vireakrothpun@gmail.com>
2022-08-24 21:53:39 +0700

    first demo

---
+++ Pointers/Demo_DynamicAllocation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int *p = new int[5]; // an array of size 5 will create in heap
+    p[0]=12;
+    p[2]=14;
+    cout<< p[1] << endl;
+
+
+    // If we're stop using it
+    delete []p;
+    p = nullptr;
+    return 0;
+}
