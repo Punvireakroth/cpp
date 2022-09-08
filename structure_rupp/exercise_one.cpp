1eeb2a0c3d5761f5d9c463645b628108adc39fb2
Punvireakroth <vireakrothpun@gmail.com>
2022-09-08 10:20:35 +0700

    added function

--- structure_rupp/exercise_one.cpp
+++ structure_rupp/exercise_one.cpp
@@ -7,8 +7,31 @@ struct Student{
     char gender;
 }vireakroth;
 
+
+void display_struct();
+
 int main()
 {
-    Student vireakroth = {18215, 19, "VireakRoth", 'M'};
-    cout<<"ID: "<< vireakroth.ID<<" Name: "<<" Gender: "<<vireakroth.gender<<" Name: "<<vireakroth.name<<" Age: "<<vireakroth.age<<endl;
+   display_struct();
+}
+
+
+void display_struct(){
+    cout << "\nInput information" << endl;
+    cout << "Name: "<<endl;
+    cin >> vireakroth.name;
+    cout << "ID: "<<endl;
+    cin >> vireakroth.ID;
+    cout << "Age: "<<endl;
+    cin >> vireakroth.age;
+    cout << "gender: "<<endl;
+    cin >> vireakroth.gender;
+    cout << "\nDisplaying Information." << endl;
+    cout << "-------------------------" << endl;
+
+
+    cout << "ID: " <<vireakroth.name << endl;
+    cout <<"Age: " << vireakroth.age << endl;
+    cout << "Name: " << vireakroth.ID <<endl;
+    cout << "Gender: " << vireakroth.gender;
 }
