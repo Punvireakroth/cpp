6221760a308109affaa24ba1ff5831533104cc3a
Punvireakroth <vireakrothpun@gmail.com>
2022-09-28 22:54:29 +0700

    getter setter

--- Class-Object/getter-setter.cpp
+++ Class-Object/getter-setter.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
 
-main(){
-    cout << "Hello world";
+class Student {
+  private:
+    // Private attribute
+    int score;
+
+  public:
+    // Setter
+    void setScore(int s) {
+      score = s;
+    }
+    // Getter
+    int getScore() {
+      return score;
+    }
+};
+
+int main() {
+  Student myObj;
+  myObj.setScore(50000);
+  cout << myObj.getScore();
+  return 0;
 }
