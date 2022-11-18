55de3d8a790c8c763ba494e4e0c42b94434cfb42
Punvireakroth <vireakrothpun@gmail.com>
2022-11-18 19:53:40 +0700

    velocity of jumping

--- final_project/runner/runner.cpp
+++ final_project/runner/runner.cpp
@@ -9,13 +9,27 @@ int main()
     // initialize the window
     InitWindow(windowWidth, windowHeight, "Runner");
 
+
+    //rect dimension 
+    const int width{50};
+    const int height{80};
+    int posY{windowHeight - height};
+    int velocity{-10};
     SetTargetFPS(60);
+
+
     while (!WindowShouldClose())
     {
         // Start drawing 
         BeginDrawing();
         ClearBackground(WHITE);
+        
+        if(IsKeyPressed(KEY_SPACE))
+        {
+            posY += velocity;
+        }
 
+        DrawRectangle(windowWidth/2, posY, width, height, BLUE);
         // Stop drawing 
         EndDrawing();
     }
