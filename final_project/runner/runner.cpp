bd5f2f6c16f9f123ca70bb4aedd31da49e2d75bc
Punvireakroth <vireakrothpun@gmail.com>
2022-11-23 21:38:32 +0700

    added structure

--- final_project/runner/runner.cpp
+++ final_project/runner/runner.cpp
@@ -1,5 +1,13 @@
 #include "raylib.h"
 
+struct AnimData
+{
+    Rectangle rec;
+    Vector2 pos;
+    int frame;
+    float updateTime;
+    float runningTime;
+};
 
 
 int main()
@@ -15,40 +23,53 @@ int main()
     const int gravity{1500};
     // Load Obstagcle 
     Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
-    Rectangle nebRec{0.0, 0.0, nebula.width/8.0, nebula.height/8.0}; //x, y, width, height 
-    Vector2 nebPos{windowWidth, windowHeight - nebRec.height}; //x = width, y = height
 
-    Rectangle neb2Rec{0.0, 0.0, nebula.width/8.0, nebula.height/8.0};
-    Vector2 neb2Pos{windowWidth + 300, windowHeight - nebRec.height};
+    // AnimData for nebula
+    AnimData nebData{
+        {0.0, 0.0, nebula.width/8.0, nebula.height/8.0},//x, y, width, height (Rec)
+        {windowWidth, windowHeight - nebula.height/8}, // Vector2 x = width, y = height
+        0, // frame
+        1.0/2.0, // update time 
+        0 //Running time
+     };
 
-    int neb2Frame{};
-    const float neb2UpdateTime{1.0/16.0};
-    float neb2RunningTime;
+    
+
+    AnimData neb2Data{
+        {0.0, 0.0, nebula.width/8.0, nebula.height/8.0}, // Rectangle x , y , width , height
+        {windowWidth + 300, windowHeight - nebData.rec.height}, // Vector
+        0,
+        1.0/16.0,
+        0.0
+    };
+    
 
     // nebula X velocity (pixel per second )
     int nebVel{-400};
 
     // Load texture of Pain Naruto
     Texture2D scarfy = LoadTexture("textures/pain.png");
-    Rectangle scarfyRec;
-    scarfyRec.width = scarfy.width/6;
-    scarfyRec.height = scarfy.height;
-    scarfyRec.x = 0;
-    scarfyRec.y = 0;
-    Vector2 scarfyPos;
-    scarfyPos.x = (windowWidth / 2) - scarfyRec.width/2;
-    scarfyPos.y = windowHeight - scarfyRec.height;
+    AnimData scarfyData;
 
-    // animation frame 
-    int frame{};
+    // pain variables
+    scarfyData.rec.width = scarfy.width/6;
+    scarfyData.rec.height = scarfy.height;
+    scarfyData.rec.x = 0;
+    scarfyData.rec.y = 0;
+    scarfyData.pos.x = windowWidth/2 - scarfyData.rec.width/2;
+    scarfyData.pos.y  = windowHeight - scarfyData.rec.height;
+    scarfyData.frame = 0;
+    scarfyData.updateTime = 1.0/12.0;
+    scarfyData.runningTime = 0.0;
 
-    bool isOnAir; 
+   
+
+
     // jump velocity (pixels/second)
-    const int jumpVelocity = 700;
+    bool isOnAir; 
 
-    // time between next animation
-    const float updateTime = 1.0/12.0;
-    float runningTime{};
+    const int jumpVelocity = 700;    
+
     //nebula animation variable
     int nebFrame{};
     const float nebUpdateTime{1.0/12.0};
@@ -68,7 +89,7 @@ int main()
         ClearBackground(WHITE);
         
         // check if sprite on the ground 
-        if(scarfyPos.y >= windowHeight - scarfyRec.height)
+        if(scarfyData.pos.y >= windowHeight - scarfyData.rec.height)
         {
             // ractangle is on the ground 
             velocity = 0;
@@ -89,57 +110,57 @@ int main()
             }
         }
         // update nebula position
-        nebPos.x += nebVel * dT;
+        nebData.pos.x += nebVel * dT;
 
         // update velocity
-        scarfyPos.y += velocity * dT;
+        scarfyData.pos.y += velocity * dT;
 
         // update the second nebula position
-        neb2Pos.x += nebVel * dT;
+        neb2Data.pos.x += nebVel * dT;
 
         // update running time
-        runningTime += dT;
-        if(runningTime >= updateTime){
-            runningTime = 0.0;
+        scarfyData.runningTime += dT;
+        if(scarfyData.runningTime >= scarfyData.updateTime){
+            scarfyData.runningTime = 0.0;
              //update animation frame
-            scarfyRec.x = frame * scarfyRec.width;
-            frame++;
-            if(frame > 5){
-                frame = 0;
+            scarfyData.rec.x = scarfyData.frame * scarfyData.rec.width;
+            scarfyData.frame++;
+            if(scarfyData.frame > 5){
+                scarfyData.frame = 0;
             }else if(isOnAir == true){
                 // do not update run animation frame when sprite is on the air
-                scarfyRec.x = scarfyRec.width;
+                scarfyData.rec.x = scarfyData.rec.width;
             }
         }
 
         //update animation frame of nebula
-        nebRunningTime += dT;
-        if(nebRunningTime >= nebUpdateTime){
+        nebData.runningTime += dT;
+        if(nebData.runningTime >= nebData.updateTime){
             nebRunningTime = 0.0;
-            nebRec.x = nebFrame * nebRec.width;
-            nebFrame++;
-            if(nebFrame > 7){
-                nebFrame = 0;
+            nebData.rec.x = nebData.frame * nebData.rec.width;
+            nebData.frame++;
+            if(nebData.frame > 7){
+                nebData.frame = 0;
             }
         }
 
         //update animation frame of nebula
-        neb2RunningTime += dT;
-        if(neb2RunningTime >= neb2UpdateTime){
-            neb2RunningTime = 0.0;
-            neb2Rec.x = neb2Frame * neb2Rec.width;
-            neb2Frame++;
-            if(neb2Frame > 7){
-                neb2Frame = 0;
+        neb2Data.runningTime += dT;
+        if(neb2Data.runningTime >= neb2Data.updateTime){
+            neb2Data.runningTime = 0.0;
+            neb2Data.rec.x = neb2Data.frame * neb2Data.rec.width;
+            neb2Data.frame++;
+            if(neb2Data.frame > 7){
+                neb2Data.frame = 0;
             }
         }
 
         // draw nebula obstagcle
-        DrawTextureRec(nebula, nebRec, nebPos, WHITE);
+        DrawTextureRec(nebula, nebData.rec, nebData.pos, WHITE);
         // draw second nebula 
-        DrawTextureRec(nebula, neb2Rec, neb2Pos, RED);
+        DrawTextureRec(nebula, neb2Data.rec, neb2Data.pos, RED);
         //draw texture pain naruto
-        DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
+        DrawTextureRec(scarfy, scarfyData.rec, scarfyData.pos, WHITE);
         // Stop drawing 
         EndDrawing();
     }
