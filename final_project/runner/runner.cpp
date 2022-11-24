f935e949b8df1c20825a7d94475d84d539a2abfc
Punvireakroth <vireakrothpun@gmail.com>
2022-11-24 07:26:54 +0700

    set up the collision

--- final_project/runner/runner.cpp
+++ final_project/runner/runner.cpp
@@ -9,13 +9,33 @@ struct AnimData
     float runningTime;
 };
 
+// function check if sprite is on the ground
+bool isOnGround(AnimData data, int windowHeight){
+    return data.pos.y >= windowHeight - data.rec.height;
+}
 
+// update pain animation frame
+AnimData updateAnimData(AnimData data, float deltaTime, int maxFrame)
+{
+    // update running time
+    data.runningTime += deltaTime;
+    if (data.runningTime >= data.updateTime){
+        data.runningTime = 0.0;
+        // update animation 
+        data.rec.x = data.frame * data.rec.width;
+        data.frame++;
+        if(data.frame > maxFrame){
+            data.frame = 0;
+        }
+    }
+    return data;
+}
 int main()
 {
     // Window width and height
 
     int windowDimensions[2];
-    windowDimensions[0] = 1400;
+    windowDimensions[0] = 1000;
     windowDimensions[1] = 720;
 
    
@@ -41,10 +61,11 @@ int main()
         nebulea[i].runningTime = 0.0;
         nebulea[i].updateTime = 0.0;
         // set position of nebula 
-        nebulea[i].pos.x = windowDimensions[0] + i * 300;
+        nebulea[i].pos.x = windowDimensions[0] + i * 400;
     }
     
-
+    // finish line variable 
+    float finishLine{nebulea[sizeOfNebulea - 1].pos.x};
     // nebula X velocity (pixel per second )
     int nebVel{-300};
 
@@ -77,10 +98,18 @@ int main()
     float nebRunningTime{};
 
     int velocity{0};
+
+    // load background
+    Texture2D background = LoadTexture("textures/far-buildings.png");
+    float bgX{};
+    Texture2D midground = LoadTexture("textures/back-buildings.png");
+    float mgX{};
+    Texture2D foreground = LoadTexture("textures/foreground.png");
+    float fgX{};
+
+    bool collision{};
+
     SetTargetFPS(60);
-
-
-
     while (!WindowShouldClose())
     {
         // Delta time (time since last frame)
@@ -89,8 +118,36 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);
         
+        // move background
+        bgX -= 20 * dT;
+        if (bgX <= -background.width*2){
+            bgX = 0.0;
+        }
+        mgX -= 40 * dT;
+        if(mgX <= -midground.width*2){
+            mgX = 0.0;
+        }
+        fgX -= 80 * dT;
+        if(fgX <= -foreground.width*2){
+            fgX = 0.0;
+        }
+        // draw the background 
+        Vector2 bg1Pos{bgX,0.0};
+        DrawTextureEx(background, bg1Pos, 0.0, 4.0, WHITE);
+        Vector2 bg2Pos{bgX + background.width*4.0, 0.0};
+        DrawTextureEx(background, bg2Pos, 0.0, 4.0, WHITE);
+
+        Vector2 mg1Pos{mgX,0.0};
+        DrawTextureEx(midground, mg1Pos, 0.0, 4.0, WHITE);
+        Vector2 mg2Pos{mgX + background.width*4.0, 0.0};
+        DrawTextureEx(midground, mg2Pos, 0.0, 4.0, WHITE);
+
+        Vector2 fg1Pos{fgX,0.0};
+        DrawTextureEx(foreground, fg1Pos, 0.0, 4.0, WHITE);
+        Vector2 fg2Pos{fgX + background.width*4.0, 0.0};
+        DrawTextureEx(foreground, fg2Pos, 0.0, 4.0, WHITE);
         // check if sprite on the ground 
-        if(scarfyData.pos.y >= windowDimensions[1] - scarfyData.rec.height)
+        if(isOnGround(scarfyData, windowDimensions[1]))
         {
             // ractangle is on the ground 
             velocity = 0;
@@ -114,24 +171,16 @@ int main()
             // update nebula's position
             nebulea[i].pos.x += nebVel * dT;
         }
-
+        // update finish line
+        finishLine += nebVel * dT;
         // update velocity
         scarfyData.pos.y += velocity * dT;
 
-        // update running time
-        scarfyData.runningTime += dT;
-        if(scarfyData.runningTime >= scarfyData.updateTime){
-            scarfyData.runningTime = 0.0;
-             //update animation frame
-            scarfyData.rec.x = scarfyData.frame * scarfyData.rec.width;
-            scarfyData.frame++;
-            if(scarfyData.frame > 5){
-                scarfyData.frame = 0;
-            }else if(isOnAir == true){
-                // do not update run animation frame when sprite is on the air
-                scarfyData.rec.x = scarfyData.rec.width;
-            }
-        }
+        // update pain animation
+       if(!isOnAir)
+       {
+        scarfyData = updateAnimData(scarfyData, dT, 5);
+       }
 
         for(int i = 0; i < sizeOfNebulea; i++){
              //update animation frame of nebula
@@ -146,18 +195,46 @@ int main()
             }
         }
 
-       
-        for(int i = 0; i < sizeOfNebulea; i++){
+        for (AnimData nebula : nebulea)
+        {
+            float pad{20};
+            Rectangle nebRec{
+                nebula.pos.x + pad,
+                nebula.pos.y + pad,
+                nebula.rec.width - 2 * pad,
+                nebula.rec.height - 2 * pad
+            };
+            Rectangle scarfyRec {
+                scarfyData.pos.x,
+                scarfyData.pos.y,
+                scarfyData.rec.width,
+                scarfyData.rec.height
+            };
+
+            if(CheckCollisionRecs(nebRec, scarfyRec)){
+                collision = true;
+            }
+        }
+
+        if(collision){
+            // hit obstacle lose the game
+        }else{
+            for(int i = 0; i < sizeOfNebulea; i++){
             // draw obstacle
             DrawTextureRec(nebula, nebulea[i].rec, nebulea[i].pos, WHITE);
-        }
+            }
        
-        //draw texture pain naruto
-        DrawTextureRec(scarfy, scarfyData.rec, scarfyData.pos, WHITE);
+            //draw texture pain naruto
+            DrawTextureRec(scarfy, scarfyData.rec, scarfyData.pos, WHITE);
+        }
+        
         // Stop drawing 
         EndDrawing();
     }
     UnloadTexture(scarfy);
     UnloadTexture(nebula);
+    UnloadTexture(background);
+    UnloadTexture(midground);
+    UnloadTexture(foreground);
     CloseWindow();
 }
